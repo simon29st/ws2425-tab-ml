8bc45103f26982a131fda33d86237be7183e5483
Simon <27553251+simon29st@users.noreply.github.com>
2025-02-21 00:29:30 +0100

    implemented training loop

--- code/benchmark.py
+++ code/benchmark.py
@@ -1,32 +1,49 @@
+import torch
 from torch import nn
+
+import pandas as pd
 
 
 # TODO: add #groups argument and split the features to their respective nets like so: https://discuss.pytorch.org/t/grouping-some-features-before-going-to-fully-connected-layers/162444/5
 class NeuralNetwork(nn.Module):
     def __init__(self, input_size, output_size, total_layers, nodes_per_hidden_layer):
         super().__init__()
-        
-        self.net = nn.Sequential()
 
         # input layer
-        self.net.append(nn.Linear(input_size, nodes_per_hidden_layer))
-        self.net.append(nn.ReLU())
+        layers = list()
+        layers.append(nn.Linear(input_size, nodes_per_hidden_layer))
+        layers.append(nn.ReLU())
 
         # hidden layers
         for _ in range(1, total_layers-1):
-            self.net.append(nn.Linear(nodes_per_hidden_layer, nodes_per_hidden_layer))
-            self.net.append(nn.ReLU())
+            layers.append(nn.Linear(nodes_per_hidden_layer, nodes_per_hidden_layer))
+            layers.append(nn.ReLU())
 
         # output layer
-        self.net.append(nn.Linear(nodes_per_hidden_layer, output_size))
-        if total_layers == 1:
-            self.net.append(nn.Sigmoid())
-        else:
-            self.net.append(nn.Softmax())
+        layers.append(nn.Linear(nodes_per_hidden_layer, output_size))
+        #if total_layers == 1:
+        #    layers.append(nn.Sigmoid())
+        #else:
+        #    layers.append(nn.Softmax())
+        
+        self.net = nn.Sequential(*layers)
     
 
     def forward(self, x):
         return self.net(x)
+
+
+class Dataset(torch.utils.data.Dataset):
+    def __init__(self, X: pd.DataFrame, y: pd.Series, class_pos: str):
+        self.X = torch.tensor(X.values, dtype=torch.float)
+        self.y = torch.zeros(len(y.index))
+        self.y[y.reset_index(drop=True) == class_pos] = 1
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx: int):
+        return self.X[idx], self.y[idx]
 
 
 def eval_nn_eagga(task_train, task_test):
