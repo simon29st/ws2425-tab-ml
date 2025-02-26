04a2bff704977f43a644712aa9d6478aa7651a46
Simon <27553251+simon29st@users.noreply.github.com>
2025-02-26 23:10:08 +0100

    moved all the training logic into a dedicated class to clean up the project

--- code/classes.py
+++ code/classes.py
@@ -7,14 +7,18 @@ from torch import nn
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.utils import resample
 from sklearn.feature_selection import mutual_info_classif
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import roc_auc_score
 
 from scipy.stats import spearmanr
 
+from nds import ndomsort
+
+from math import comb
 from copy import deepcopy
-from collections import defaultdict
+from datetime import datetime, timedelta
 
 
 class NeuralNetwork(nn.Module):
@@ -460,3 +464,356 @@ class Prob:
     @staticmethod
     def fit_decision_tree(data):
         pass
+
+
+class EAGGA:
+    def __init__(self, oml_dataset, class_positive, hps: dict[str, tuple | int | float], batch_size: int, patience: int, secs_per_fold: int, secs_total: int):
+        self.data, _, self.categorical_indicator, _ = oml_dataset.get_data()
+        self.class_column = oml_dataset.default_target_attribute
+        self.class_positive = class_positive
+
+        if self.data.isnull().values.any():  # from manual checks: dataset 'jm1' has NaN values in 5 rows -> simply remove
+            print(f'Drop {self.data.loc[self.data.isnull().any(axis=1), :].shape[0]} NaN values of dataset {oml_dataset.name}')
+            self.data = self.data.dropna(axis=0, how='any')
+            self.data = self.data.reset_index(drop=True)
+        
+        self.hps = hps
+
+        # outer split, holdout testing
+        self.data_train_val, self.data_test = train_test_split(
+            self.data,
+            train_size=self.hps['holdout_train_size'],
+            shuffle=True,
+            stratify=self.data.loc[:, self.class_column]
+        )
+        # reset indices as StratifiedKFold assumes consecutive index
+        self.data_train_val = self.data_train_val.reset_index(drop=True)
+        self.data_test = self.data_test.reset_index(drop=True)
+
+        # inner split, k-fold cross validation
+        self.cv = StratifiedKFold(
+            n_splits=self.hps['cv_k'],
+            shuffle=True
+        )
+
+        self.batch_size = batch_size
+        self.patience = patience
+
+        self.secs_per_fold = secs_per_fold
+        self.secs_total = secs_total
+
+        self.monotonicity_clipper = WeightClipper(0, None)  # enforce monotonicity by clipping weights to [0, infty) after each epoch (in def train)
+
+        self.offspring = self.init_population()
+        self.population = list()
+
+
+    def init_population(self):
+        print('Starting init population')
+        population_layers = Prob.r_trunc_geom(Prob.p_sample_hps, self.hps['mu'], self.hps['total_layers'][0], self.hps['total_layers'][1])
+        population_nodes = Prob.r_trunc_geom(Prob.p_sample_hps, self.hps['mu'], self.hps['nodes_per_hidden_layer'][0], self.hps['nodes_per_hidden_layer'][1])
+
+        all_features = list(i for i in range(len(self.data_train_val.columns) - 1))
+        population_features_included = [GroupStructure.detector_features(self.data_train_val, self.categorical_indicator, self.class_column) for _ in range(self.hps['mu'])]
+        population_features_excluded = [list(set(all_features) - set(features_included)) for features_included in population_features_included]
+        population_interactions = [GroupStructure.detector_interactions(self.data_train_val, features_included, self.class_column) for features_included in population_features_included]
+        population_monotonicity_constraints = [GroupStructure.detector_monotonicity(self.data_train_val, groups_without_monotonicity, self.class_column) for groups_without_monotonicity in population_interactions]
+        print('Finished init population')
+
+        return [{
+            'total_layers': population_layers[i].item(),
+            'nodes_per_hidden_layer': population_nodes[i].item(),
+            'group_structure': GroupStructure(
+                all_features,
+                population_monotonicity_constraints[i][1],
+                population_features_excluded[i],
+                *population_monotonicity_constraints[i][0]
+            )
+        } for i in range(self.hps['mu'])]
+
+
+    def start_eagga(self):
+        time_start = datetime.now()
+        print(f'Start EAGGA at {time_start.isoformat()}')
+
+        gen = 0
+        while(datetime.now() < time_start + timedelta(seconds=self.secs_total)):
+            print(f'Generation {gen+1}, evaluate {len(self.offspring)} individuals')
+
+            for individual in self.offspring:
+                individual['metrics'] = self.run_cv(individual)
+                self.population.append(individual)
+
+                if datetime.now() >= time_start + timedelta(seconds=self.secs_total):
+                    break  # don't train any more individuals if time ran out
+            
+            ranks_nds = ndomsort.non_domin_sort(
+                [individual['metrics']['performance']['mean'] for individual in self.population],
+                get_objectives=lambda performance_tup: (1 - performance_tup[0], *[performance_tup[i] for i in range(1, len(performance_tup))]),  # compute pareto fronts w.r.t. reference (worst) point (0, 1, 1, 1)
+                only_front_indices=True
+            )
+            for i in range(len(self.population)):
+                self.population[i]['rank_nds'] = ranks_nds[i]
+            self.population = sorted(self.population, key=lambda individual: individual['rank_nds'])[:self.hps['mu']]  # ascending order, lower ranks are better, choose best mu individuals
+            
+            if datetime.now() >= time_start + timedelta(seconds=self.secs_total):
+                break  # don't generate offspring if time ran out anyway
+            self.offspring = self.generate_offspring()
+            
+            gen += 1
+        print(f'Finished EAGGA at {datetime.now().isoformat()}')   
+
+
+    # runs k-fold cv training with stopping early
+    def run_cv(self, individual):
+        total_layers = individual['total_layers']
+        nodes_per_hidden_layer = individual['nodes_per_hidden_layer']
+        group_structure = individual['group_structure']
+        print(f'Running {self.hps['cv_k']}-fold CV for individual: {total_layers} total layers, {nodes_per_hidden_layer} nodes per hidden layer, gs: {group_structure}')
+
+        metrics = {
+            'performance': list(),
+            'epochs': list()
+        }
+
+        for fold, (indices_train, indices_val) in enumerate(self.cv.split(X=self.data_train_val, y=self.data_train_val.loc[:, self.class_column])):
+            data_train_stop_early = self.data_train_val.loc[indices_train, :]
+            data_train, data_stop_early = train_test_split(
+                data_train_stop_early,
+                train_size=0.8,
+                shuffle=True,
+                stratify=data_train_stop_early.loc[:, self.class_column]
+            )
+            dataset_train = Dataset(
+                X=data_train.loc[:, data_train.columns != self.class_column],
+                y=data_train.loc[:, self.class_column],
+                class_pos=self.class_positive,
+                group_structure=group_structure
+            )
+            dataset_stop_early = Dataset(
+                X=data_stop_early.loc[:, data_stop_early.columns != self.class_column],
+                y=data_stop_early.loc[:, self.class_column],
+                class_pos=self.class_positive,
+                group_structure=group_structure
+            )
+
+            data_val = self.data_train_val.loc[indices_val, :]
+            dataset_val = Dataset(
+                X=data_val.loc[:, data_val.columns != self.class_column],
+                y=data_val.loc[:, self.class_column],
+                class_pos=self.class_positive,
+                group_structure=group_structure
+            )
+
+            model = NeuralNetwork(
+                group_structure=group_structure,
+                output_size=1,  # we only use binary datasets
+                total_layers=total_layers,
+                nodes_per_hidden_layer=nodes_per_hidden_layer
+            )
+
+            optimizer = torch.optim.AdamW(model.parameters())
+            loss_fn = nn.BCEWithLogitsLoss()
+
+            model, optimal_epoch = self.train(optimizer, loss_fn, model, dataset_train, dataset_stop_early)
+
+            metrics['performance'].append(self.eval(loss_fn, model, dataset_val))
+            metrics['epochs'].append(optimal_epoch)
+            print(f'Fold {fold + 1}/{self.cv.get_n_splits()} | trained for {optimal_epoch} epochs | metrics: {metrics['performance'][-1]}')  # TODO: remove
+
+        # TODO: no need to record NF, NI, NNM over folds and compute mean + var for them, as they stay the same in each fold
+        return {
+            'performance': {
+                'mean': np.mean(metrics['performance'], axis=0),
+                'var': np.var(metrics['performance'], axis=0),
+                'folds': metrics['performance']
+            },
+            'epochs': {
+                'mean': np.mean(metrics['epochs'], axis=0),
+                'folds': metrics['epochs']
+            }
+        }
+
+
+    # does one training fold
+    # cf. https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
+    def train(self, optimizer, loss_fn, model: NeuralNetwork, dataset_train: Dataset, dataset_stop_early: Dataset) -> tuple:
+        loader_train = torch.utils.data.DataLoader(dataset_train, batch_size=self.batch_size, shuffle=True)
+
+        epoch = 0
+        loss_history_early_stopping = list()
+        time_start = datetime.now()
+        while(datetime.now() < time_start + timedelta(seconds=self.secs_per_fold)):
+            model.train()  # training mode, put here as we switch to eval mode at the end of each epoch in def stop_early
+            running_epoch_loss = 0
+
+            for batch_input, batch_target in loader_train:  # divide data in mini batches
+                optimizer.zero_grad()  # set gradients to 0
+                batch_output = model(*batch_input).flatten()  # expand batch_input as it is a list of tuples (Dataset getter splits according to group structure)
+
+                batch_loss = loss_fn(batch_output, batch_target)
+                running_epoch_loss += batch_loss.detach().item()
+                batch_loss.backward()  # compute gradients
+
+                optimizer.step()  # update weights
+
+            running_epoch_loss /= len(loader_train)
+
+            if self.monotonicity_clipper is not None:  # used for monotonicity constraint
+                for i, (_, monotonicity_constraint) in enumerate(model.get_group_structure().get_included_groups()):
+                    if monotonicity_constraint == 1:
+                        model.networks[i].apply(self.monotonicity_clipper)  # applies weight clipper recursively to network + its children, cf. https://pytorch.org/docs/stable/generated/torch.nn.Module.html#torch.nn.Module.apply
+            
+            should_stop_early, optimal_epoch = self. stop_early(loss_history_early_stopping, loss_fn, model, dataset_stop_early)
+            if should_stop_early:
+                return model, optimal_epoch
+            epoch += 1
+        
+        return model, epoch
+    
+
+    def stop_early(self, loss_history: list, loss_fn, model: NeuralNetwork, dataset_stop_early: Dataset) -> tuple:
+        # stopping criterion: mean of 'patience' previous losses over [t-patience, t] < current loss at t+1
+        # if True -> go back to min loss within [t-patience, t]
+        loss = self.eval(loss_fn, model, dataset_stop_early, only_loss=True)
+        loss_history.append(loss)
+
+        if len(loss_history) > self.patience and np.mean(loss_history[-self.patience-1:-1]) < loss:
+            mask = np.ones_like(loss_history)
+            mask[:-self.patience-1] = np.inf
+            optimal_epoch = np.argmin(mask * loss_history) + 1  # +1 to make it 1-based (as opposed to 0-based from np.argmin indexing)
+            print(f'Stop early: {np.mean(loss_history[-self.patience-1:-1])} < {loss}, optimal epoch {optimal_epoch}')
+            return True, optimal_epoch
+        
+        return False, -1
+    
+
+    # cf. https://pytorch.org/tutorials/beginner/introyt/trainingyt.html
+    def eval(self, loss_fn, model: NeuralNetwork, dataset_eval: Dataset, only_loss: bool = False) -> tuple | float:
+        loader_eval = torch.utils.data.DataLoader(dataset_eval, batch_size=self.batch_size)
+        
+        model.eval()  # eval mode
+        running_loss = 0
+
+        batch_predictions = list()
+        batch_targets = list()
+        for batch_input, batch_target in loader_eval:
+            with torch.no_grad():
+                batch_output = model(*batch_input).flatten()
+            
+            batch_loss = loss_fn(batch_output, batch_target)
+            batch_prediction = torch.sigmoid(batch_output)
+
+            batch_predictions.append(batch_prediction)
+            batch_targets.append(batch_target)
+
+            running_loss += batch_loss.detach().item()
+        running_loss /= len(loader_eval)
+
+        if only_loss:
+            return running_loss
+
+        # compute AUC
+        predictions = torch.cat(batch_predictions)
+        targets = torch.cat(batch_targets)
+        auc = roc_auc_score(targets, predictions)
+        
+        # compute interpretability metrics
+        #   NF via group structure: relative # of included features
+        #   NI via group structure:
+        #       for each group with > 1 features: sum over range from 1 to (n - 1 = # of features in group - 1) -> # handshakes in party -> same as {n choose 2}
+        #       then divide by # of all possible interactions {all_features choose 2}
+        #   NNM via
+        gs = model.get_group_structure()
+        num_features_all = len(gs.get_all_features())
+        num_features_included = len(gs.get_included_features())
+        num_features_unconstrained = len(gs.get_unconstrained_features())
+
+        nf = num_features_included / num_features_all
+        ni = 0
+        for (features, _) in gs.get_included_groups():
+            num_features_in_group = len(features)
+            if num_features_in_group > 1:
+                ni += comb(num_features_in_group, 2)  # ni += sum(range(1, num_features_in_group))
+        ni /= comb(num_features_all, 2)
+        nnm = num_features_unconstrained / num_features_all  # via gs: (# of features in groups without monotonicity constraint) / (total # of features)
+        
+        return running_loss, auc.item(), nf, ni, nnm
+
+
+    def generate_offspring(self):
+        offspring = list()
+
+        while len(offspring) <= self.hps['lambda']:
+            id_parent_1 = self.binary_tournament()
+            id_parent_2 = self.binary_tournament()
+            parent_1 = self.population[id_parent_1]
+            parent_2 = self.population[id_parent_2]
+
+            child_1 = deepcopy(parent_1)
+            child_2 = deepcopy(parent_2)
+            del child_1['metrics']
+            del child_2['metrics']
+
+            # EA on hyperparams
+            if Prob.should_do(Prob.p_ea_crossover_overall):  # uniform crossover
+                if Prob.should_do(Prob.p_ea_crossover_param):
+                    child_1['total_layers'] = parent_2['total_layers']
+                    child_2['total_layers'] = parent_1['total_layers']
+                if Prob.should_do(Prob.p_ea_crossover_param):
+                    child_1['nodes_per_hidden_layer'] = parent_2['nodes_per_hidden_layer']
+                    child_2['nodes_per_hidden_layer'] = parent_1['nodes_per_hidden_layer']
+            for child in [child_1, child_2]:  # Gaussian mutation
+                if Prob.should_do(Prob.p_ea_mutate_overall):
+                    if Prob.should_do(Prob.p_ea_mutate_param):
+                        child['total_layers'] = EAGGA.ea_mutate_gaussian(child['total_layers'], self.hps['total_layers'][0], self.hps['total_layers'][1])
+                        child['total_layers'] = round(child['total_layers'])
+                    if Prob.should_do(Prob.p_ea_mutate_param):
+                        child['nodes_per_hidden_layer'] = EAGGA.ea_mutate_gaussian(child['nodes_per_hidden_layer'], self.hps['nodes_per_hidden_layer'][0], self.hps['nodes_per_hidden_layer'][1])
+                        child['nodes_per_hidden_layer'] = round(child['nodes_per_hidden_layer'])
+
+            # GGA on group structure
+            if Prob.should_do(Prob.p_gga_crossover):  # crossover
+                gs_1 = child_1['group_structure']
+                gs_2 = child_2['group_structure']
+                gs_1, gs_2 = GroupStructure.gga_crossover(gs_1, gs_2)
+                child_1['group_structure'] = gs_1
+                child_2['group_structure'] = gs_2
+            for child in [child_1, child_2]:  # mutate
+                if Prob.should_do(Prob.p_gga_mutate_overall):
+                    child['group_structure'].gga_mutate()
+
+            offspring.append(child_1)
+            offspring.append(child_2)
+
+        return offspring[:self.hps['lambda']]  # in case offsping is longer than lambda (while-loop always adds 2 individuals), discard offspring after lambda
+    
+
+    def binary_tournament(self):
+        '''
+        steps
+        (1) sample (without replacement) two random ids from population
+        (2) non-dominated sorting -> rank pareto fronts
+        (3)
+        (a) if the sampled ids are in differently ranked pareto fronts -> return id with lower pareto front rank
+        (b) else (sampled ids are from same pareto front)
+        (i)     compute crowding distance
+        (ii)    return id with larger crowding distance
+        '''
+        # (1)
+        ids = np.random.choice(a=len(self.population), size=2, replace=False)#.tolist()
+        # (2) ranks_nds taken from EA loop
+        # (3)
+        if self.population[ids[0]]['rank_nds'] != self.population[ids[1]]['rank_nds']:  # (a)
+            return sorted(ids, key=lambda id: self.population[id]['rank_nds'])[0]  # ascending order, lower ranks are better
+        else:  # (b)
+            # skip (3, b, i+ii) for now, TODO: if time -> implement crowding distance, computation cf. https://de.mathworks.com/matlabcentral/fileexchange/65809-on-the-calculation-of-crowding-distance
+            return ids[0]
+    
+
+    @staticmethod
+    def ea_mutate_gaussian(val, lower, upper):
+        val = (val - lower) / (upper - lower)
+        val = np.random.normal(loc=val, scale=0.1, size=1).item()
+        val = (val * (upper - lower)) + lower
+        return min(max(val, lower), upper)
